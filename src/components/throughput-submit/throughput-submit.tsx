34ad605b56c0972da5e9e3f183ce613a7e0b9a6e
Shane98c <shane98c@GMAIL.COM>
2020-10-09 10:44:40 -0500

    orcid url

--- src/components/throughput-submit/throughput-submit.tsx
+++ src/components/throughput-submit/throughput-submit.tsx
@@ -8,9 +8,12 @@ import { Component, h } from "@stencil/core";
 export class ThroughputSubmit {
   handleClick() {
     console.log("clicks");
+    console.log(window.location.href);
+    window.location.href =
+      "https://orcid.org/oauth/authorize?client_id=APP-STQ2Q8H8Q8FRIX7N&response_type=code&scope=openid&redirect_uri=" +
+      window.location.href;
   }
-  url: string =
-    "https://orcid.org/signin?oauth&response_type=token&redirect_uri=https://orcid.github.io/orcid-auth-widget/widget.html&client_id=APP-34IBF14LOTOGDJZZ&scope=openid%20/activities/update";
+
   render() {
     return <button onClick={this.handleClick}>tesst</button>;
   }
