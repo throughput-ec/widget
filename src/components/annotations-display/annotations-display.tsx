a34b647e6affd8e9b1645cd518c4516a03531760
Shane98c <shane98c@GMAIL.COM>
2020-12-04 17:43:47 -0600

    WIP

--- src/components/annotations-display/annotations-display.tsx
+++ src/components/annotations-display/annotations-display.tsx
@@ -3,10 +3,9 @@ import { Component, Prop, h, Listen } from "@stencil/core";
 @Component({
   tag: "annotations-display",
   styleUrl: "annotations-display.css",
-  assetsDirs: ['assets'],
+  assetsDirs: ["assets"],
   shadow: true,
 })
-
 export class AnnotationsDisplay {
   @Prop() authenticated: boolean = false;
   @Prop() addAnnotation: boolean = false;
@@ -40,23 +39,41 @@ export class AnnotationsDisplay {
     return (
       <div class="overlay">
         <div class="annotation_list">
-          <div class="header">
-            <img src="https://raw.githubusercontent.com/throughput-ec/throughput-ec.github.io/master/resources/throughput.png" height="100" width="222"/>
-            List of Annotations
+          <div class="closeContainer">
             <a href="#" id="close_x" class="close" />
           </div>
+          <div class="header">
+            {/* <img
+              src="https://raw.githubusercontent.com/throughput-ec/throughput-ec.github.io/master/resources/throughput.png"
+              height="100"
+              width="222"
+            /> */}
+            Throughput Annotations
+          </div>
           <div class="body">
-            { !this.readOnlyMode ?
-              (
-                !this.authenticated ? <orcid-connect /> :
-                !this.addAnnotation ? <button id="add_button" class="add_button">+ Add Annotation</button> :
-                  <div>
-                    <textarea onInput={(event) => this.updateAnnotationText(event)}>Add your annotation here.</textarea>
-                    <button id="submit_button" class="add_button">Submit</button>
-                    <button id="cancel_button" class="cancel_button">Cancel</button>
-                  </div>
-               ) : null
-            }
+            {!this.readOnlyMode ? (
+              !this.authenticated ? (
+                <orcid-connect />
+              ) : !this.addAnnotation ? (
+                <button id="add_button" class="add_button">
+                  + Add Annotation
+                </button>
+              ) : (
+                <div>
+                  <textarea
+                    onInput={(event) => this.updateAnnotationText(event)}
+                  >
+                    Add your annotation here.
+                  </textarea>
+                  <button id="submit_button" class="add_button">
+                    Submit
+                  </button>
+                  <button id="cancel_button" class="cancel_button">
+                    Cancel
+                  </button>
+                </div>
+              )
+            ) : null}
             {this.annotations.map((annotation) => (
               <div class="annotation_item">
                 <div class="annotation_author">{annotation.author}</div>
--- src/components/orcid-connect/orcid-connect.tsx
+++ src/components/orcid-connect/orcid-connect.tsx
@@ -3,15 +3,28 @@ import { Component, h } from "@stencil/core";
 @Component({
   tag: "orcid-connect",
   styleUrl: "orcid-connect.css",
-  shadow: true
+  shadow: true,
 })
-
 export class OrcidConnect {
   openORCID() {
-    window.open("https://sandbox.orcid.org/oauth/authorize?response_type=token&redirect_uri=http%3A%2F%2Flocalhost%3A3333%2F&client_id=APP-EDLUYOOYTPV3RMXO&scope=openid&nonce=ThroughputWidgetNonce", "_self");
+    window.open(
+      "https://sandbox.orcid.org/oauth/authorize?response_type=token&redirect_uri=http%3A%2F%2Flocalhost%3A3333%2F&client_id=APP-EDLUYOOYTPV3RMXO&scope=openid&nonce=ThroughputWidgetNonce",
+      "_self"
+    );
   }
 
   render() {
-    return <button id="connect-orcid-button" onClick={this.openORCID}><img id="orcid-id-icon" src="https://orcid.org/sites/default/files/images/orcid_24x24.png" width="24" height="24" alt="ORCID iD icon"/>Register or Connect your ORCID iD</button>;
+    return (
+      <button id="connect-orcid-button" onClick={this.openORCID}>
+        To Submit Annotations - Connect your ORCID iD
+        <img
+          id="orcid-id-icon"
+          src="https://orcid.org/sites/default/files/images/orcid_24x24.png"
+          width="24"
+          height="24"
+          alt="ORCID iD icon"
+        />
+      </button>
+    );
   }
 }
