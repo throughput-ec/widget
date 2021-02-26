ca6192281d7c2e3b932e7c491aabaf3d5c310fb8
Brian G <sorghumking@yahoo.com>
2021-02-26 16:38:31 -0600

    Use await vs .then to return success from submitAnnotation(); switch vs if-elses in handleClick(), catch and report unexpected clicks

--- src/components/annotations-display/annotations-display.tsx
+++ src/components/annotations-display/annotations-display.tsx
@@ -8,7 +8,7 @@ import { Component, Prop, h, Listen } from "@stencil/core";
 })
 export class AnnotationsDisplay {
   @Prop() authenticated: boolean = false;
-  @Prop() addAnnotation: boolean = false;
+  @Prop() addAnnotation: boolean = false; // show add annotation UI
   @Prop() identifier: string;
   @Prop() additionalType: string;
   @Prop() link: any;
@@ -19,15 +19,26 @@ export class AnnotationsDisplay {
   DEFAULT_ANNOTATION_TEXT: string = "Enter your annotation here.";
 
   @Listen("click")
-  handleClick(ev) {
-    if (ev.composedPath()[0].id == "add_button") {
-      this.addAnnotation = true;
-    } else if (ev.composedPath()[0].id == "submit_button") {
-      console.log("Submit clicked");
-      this.submitAnnotation();
-      this.addAnnotation = false; // TODO: only close if submit successful?
-    } else if (ev.composedPath()[0].id == "cancel_button") {
-      this.addAnnotation = false;
+  async handleClick(ev) {
+    const clicked_id = ev.composedPath()[0].id;
+    switch (clicked_id) {
+      case "add_button":
+        this.addAnnotation = true;
+        break;
+      case "submit_button":
+        console.log("Submit clicked");
+        var success = await this.submitAnnotation();
+        this.addAnnotation = !success; // close add annotation UI if submit succeeds
+        break;
+      case "cancel_button":
+        this.addAnnotation = false;
+        break;
+      case "close_x":
+        // Ignore close_x here, it's handled in DataDisplay.handleClick(), which
+        // is called after this.handleClick(). Otherwise we hit default below.
+        break;
+      default:
+        console.error("Unhandled click, id = ", clicked_id);
     }
   }
 
@@ -42,9 +53,9 @@ export class AnnotationsDisplay {
     }
   }
 
-  submitAnnotation() {
+  // POST new annotation to Throughput
+  async submitAnnotation() {
     console.log("Submitting annotation text: ", this.annotationText);
-    // POST new annotation to Throughput
     const annotation = {
       dbid: this.identifier,
       orcid: "0000-0002-6229-7677", // TODO: hard-coding
@@ -54,23 +65,22 @@ export class AnnotationsDisplay {
       token: this.token
     };
     const url = "https://throughputdb.com/api/widget/";
-    fetch(url, {
+    const response = await fetch(url, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(annotation)
-    }).then((response) => {
-      response.json().then((json) => {
-        console.log(json);
-        const success = json.status && json.status === "success"; // TODO: how to return success var from submitAnnotation?
-        if (!success) {
-          const errmsg = "Submit annotation failed: " + (json.message ? json.message : "[no message provided]");
-          console.error(errmsg);
-          alert(errmsg);
-        }
-      });
-    });
+    })
+    const json_thing = await response.json();
+    console.log(json_thing);
+    const success = json_thing.status && json_thing.status === "success";
+    if (!success) {
+      const errmsg = "Submit annotation failed: " + (json_thing.message ? json_thing.message : "[no message provided]");
+      console.error(errmsg);
+      alert(errmsg);
+    }
+    return success;
   }
 
   getFormattedDate(date) {
